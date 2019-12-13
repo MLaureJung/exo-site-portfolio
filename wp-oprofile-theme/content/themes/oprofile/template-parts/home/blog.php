e62e206ea705078b95fbefd6d6727bf1461111d0
MLaureJung <mlaure.jung@gmail.com>
2019-12-13 13:56:31 +0100

    effets sur intro buttons au survol

--- wp-oprofile-theme/content/themes/oprofile/template-parts/home/blog.php
+++ wp-oprofile-theme/content/themes/oprofile/template-parts/home/blog.php
@@ -10,12 +10,13 @@ $wpqueryArticles = new WP_Query($args);
 <section id="blog" class="posts">
   <?php if ($wpqueryArticles->have_posts()) : while ($wpqueryArticles->have_posts()) : $wpqueryArticles->the_post(); ?>
     <article class="post" style=" background-image: url(<?php the_post_thumbnail_url(); ?>); background-size: cover;">
+      <div class="post__overlay"></div>  
       <div class="post__title">
-        <h3><?php the_title(); ?></h3>
-      </div>
-      <div class="post__content">
-        <?php the_content(); ?>
-      </div>
+          <h3><?php the_title(); ?></h3>
+        </div>
+        <div class="post__content">
+          <?php the_content(); ?>
+        </div>
     </article>
    <?php endwhile; endif; ?>
 </section>
