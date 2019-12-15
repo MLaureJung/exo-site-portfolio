1e507b0f89ceb4711d94229ecaf8b63c8a3014d2
MLaureJung <mlaure.jung@gmail.com>
2019-12-15 11:44:55 +0100

    affichage slug category pour font-awesome

--- wp-oprofile-theme/content/themes/oprofile/template-parts/home/grid.php
+++ wp-oprofile-theme/content/themes/oprofile/template-parts/home/grid.php
@@ -1,5 +1,6 @@
 
 <?php
+
   $args = [
     'post_type' => 'post',
     'posts_per_page' => 6,
@@ -11,11 +12,13 @@ $wpqueryArticles = new WP_Query($args);
 
 ?>
     <section class="grids">
-      <?php if ($wpqueryArticles->have_posts()) : while ($wpqueryArticles->have_posts()) : $wpqueryArticles->the_post(); ?>
+      <?php if ($wpqueryArticles->have_posts()) : while ($wpqueryArticles->have_posts()) : $wpqueryArticles->the_post(); 
+      $cats =  get_the_category();
+        $cat = $cats[0];?>
         <div class="grid">
           <div class="grid__title">
           <div class="grid__button">
-            <i class="fa fa-user" aria-hidden="true"></i>
+            <i class="fa fa-<?= $cat->slug; ?>  " aria-hidden="true"></i>
           </div>
             <h3><?php the_title(); ?></h3>
           </div>
